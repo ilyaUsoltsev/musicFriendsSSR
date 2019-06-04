4f4fc6457b02f1ee8eef23281b273ee2dc20f72e
ilya <Usoltsev@Ilyas-MacBook-Pro.local>
2019-06-04 13:51:43 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('works!')
+})
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log('Listening...')
+});
